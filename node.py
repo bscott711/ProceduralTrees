51e304934d47e3d855a10119b50bb2e378ad714a
Abram <58718191+4b3c@users.noreply.github.com>
2024-05-13 18:12:31 -0700

    cleaned up and somewhat organized the code

--- node.py
+++ node.py
@@ -1,5 +1,12 @@
+import pygame
+import math
 import random
 from leaf import Leaf
+
+
+brown0 = (133, 94, 41)
+brown1 = (102, 68, 20)
+
 
 class Node:
 	
@@ -32,3 +39,81 @@ class Node:
 		self.age -= 3
 		if self.left != None: self.left.age -= 3
 		if self.right != None: self.right.age -= 3
+
+
+# Node functions
+def copy(node: Node) -> Node:
+	if node is None:
+		return None
+	new_node = Node(node.age, node.length, node.angle)
+	new_node.left = copy(node.left)
+	new_node.right = copy(node.right)
+	return new_node
+
+
+def count(node: Node) -> int: # Returns the number of child nodes (incudes itself)
+	if (node == None):
+		return 0
+	
+	return 1 + count(node.left) + count(node.right)
+
+
+def youngest(node: Node) -> tuple[int, Node]: # Returns the age and node
+	if (node == None):
+		return [10000, None]
+		
+	left = youngest(node.left)
+	right = youngest(node.right)
+
+	if (node.age < left[0] and node.age < right[0]):
+		return (node.age, node)
+	else:
+		return left if left[0] < right[0] else right
+
+
+def draw_parallel_lines(start, stop, perp_angle, width, window): # Perp angle needs to be in radians https://www.desmos.com/calculator/hh236r60m7
+	for i in range(round(-width / 2), round(width / 2) + 1, 1):
+		new_start = (start[0] + (i * math.cos(perp_angle)), start[1] + (-i * math.sin(perp_angle)) - (abs(i)**(2/3)))
+		new_stop = (stop[0] + (i * math.cos(perp_angle)), stop[1] + (-i * math.sin(perp_angle)) + (abs(i)**(2/3)))
+		
+		if i < -1:
+			brown = brown1
+		else:
+			brown = brown0
+		pygame.draw.line(window, brown, new_start, new_stop, 3)
+
+
+def get_position(start: tuple[float ,float], radius: float, angle: float): # Takes rectangular and polar coords and adds them
+	return ((start[0] + (radius * math.cos(angle))), (start[1] - (radius * math.sin(angle))))
+
+
+def draw_branches(node: Node, start: tuple[float, float], window: pygame.Surface) -> None: # Draws every node in a tree, along with connections
+	if (node == None):
+		return
+	pos = get_position(start, node.length, math.radians(node.angle))
+	width = count(node)**(3/4)
+
+	pygame.draw.circle(window, brown0, pos, width * 0.6) # This brown circle colors in gaps between braches that are very bent
+	draw_parallel_lines(start, pos, math.radians(node.angle + 90), width, window) # Draws the branch for each node
+	# pygame.draw.line(window, (255, 255, 255), start, pos, 2) # Skeleton of the tree for testing purposes
+	draw_branches(node.left, pos, window) # Recursively draws node's left and right children
+	draw_branches(node.right, pos, window)
+
+
+def draw_leaves(node: Node, start: tuple[float, float], window: pygame.Surface) -> None: # Draws every node in a tree, along with connections
+	if (node == None):
+		return
+	pos = get_position(start, node.length, math.radians(node.angle))
+
+	if (count(node) < 6): window.blit(node.leaves, (pos[0] - 32, pos[1] - 32)) # If the node has < 10 children draw leaves
+	draw_leaves(node.left, pos, window) # Recursively draws node's left and right children
+	draw_leaves(node.right, pos, window)
+	if (count(node) < 4): window.blit(node.leaves, (pos[0] - 32, pos[1] - 32)) # If the node is extremely close to the edge, it also draws leaves on top
+		
+
+def random_child(node: Node) -> Node: # Picks a random node from the children of a root
+	if (node == None or node.left == None or node.right == None):
+		return None
+	
+	choices = [node, random_child(node.left), random_child(node.left), random_child(node.right), random_child(node.right)]
+	return random.choice([valid for valid in choices if valid != None]) # Only returns valid choices (Not None and having 2 children)
